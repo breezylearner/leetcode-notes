35e8e9673765bd17a6b4cef8d38ebc6a3611559e
breezylearner <skylimit123@foxmail.com>
2021-03-08 00:40:00 +0800

    2021-3-7

---
+++ src/leetcode033-Search_in_Rotated_Sorted_Array.js
@@ -0,0 +1,32 @@
+// 直接用indexOf进行判断
+var Mysearch = function (nums, target) {
+  return nums.indexOf(target);
+};
+// 解法1
+var search2 = function (nums, target) {
+  if (!nums || nums.length === 0) return -1;
+  let start = 0;
+  let end = nums.length - 1;
+  // 跳出循环两点：（1）数组中没有→返回-1 （2）找到目标数
+  while (start <= end) {
+    let mid = (start + end) >> 1;
+    if (nums[start] === target) return start; //target在两端
+    if (nums[end] === target) return end;
+    if (nums[mid] === target) return mid; //target在中间
+    // 说明前半部分有序
+    if (nums[start] < nums[mid]) {
+      if (nums[start] < target && target < nums[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    } else {
+      if (nums[mid] < target && target < nums[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
